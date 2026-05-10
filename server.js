deb93865f0b121fe56b5ebbdfb60a47f478b4b8f
GhaoualIbtissam <ighaoual@gmail.com>
2026-05-10 16:59:44 +0200

    config locale et nettoyage server.js

--- server.js
+++ server.js
@@ -7,21 +7,23 @@ const dashboardRoutes = require('./routes/dashboardRoutes');
 dotenv.config();
 const app = express();
 
-// Middlewares
+
 app.use(cors()); 
 app.use(express.json());
 
-// Routes
+
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/tasks', require('./routes/task')); 
-app.use('/api/dashboard', require('./routes/dashboardRoutes'));
-
+app.use('/api/dashboard', dashboardRoutes);
 app.get('/', (req, res) => res.send("Serveur TaskFlow Opérationnel"));
 
-// Connexion MongoDB
-mongoose.connect(process.env.MONGO_URI)
+
+const mongoURI = "mongodb://127.0.0.1:27017/TaskFlow";
+mongoose.connect(mongoURI)
     .then(() => {
-        console.log('✅ Connexion à MongoDB réussie !');
-        app.listen(5000, () => console.log("✅ Serveur sur port 5000"));
+        console.log('✅ Connexion à MongoDB Locale réussie !');
+        if (!app.listening) {
+            app.listen(5000, () => console.log("✅ Serveur running on port 5000"));
+        }
     })
-    .catch(err => console.error('❌ Erreur MongoDB :', err));
+    .catch(err => console.error('❌ Erreur MongoDB Atlas :', err));
