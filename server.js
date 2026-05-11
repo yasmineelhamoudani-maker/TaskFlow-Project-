4bbebd09e87a93ae23f5c737fe90d4385cf1bed9
GhaoualIbtissam <ighaoual@gmail.com>
2026-05-12 01:53:46 +0200

    Sauvegarde de ma configuration locale et frontend

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express();
 // Middlewares
 app.use(cors()); 
 app.use(express.json());
+app.use(express.static('public'));
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
